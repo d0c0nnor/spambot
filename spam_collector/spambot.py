5b8f374b438a1e8c1112e7b37c1aa754af6128b0
Tiziano Santoro <tiziano88@gmail.com>
2012-09-15 17:20:11 +0100

    Adding spam data

--- spam_collector/spambot.py
+++ spam_collector/spambot.py
@@ -4,6 +4,7 @@ import re
 import os
 import glob
 import sys
+import uuid
 
 def remove_html_tags(data):
   p = re.compile(r'<.*?>')
@@ -17,11 +18,14 @@ def remove_links(data):
   p = re.compile(r'http\S+')
   return p.sub('', data)
 
+data = "data"
+#text = 
+
 path = sys.argv[1]
 
-os.chdir(path)
-for files in os.listdir("."):
-  content = open(files, 'r').read()
+#os.chdir(path)
+for files in os.listdir(path):
+  content = open(path + '/' + files, 'r').read()
   msg = email.message_from_string(content)
   if msg.is_multipart():
     payload = msg.get_payload(0)
@@ -33,9 +37,16 @@ for files in os.listdir("."):
     content = remove_links(content)
     content = remove_html_tags(content)
     print content
+    print "\n\n"
+  continue
   s = raw_input("press 'n' for next, 's' for speak...\n\n\n")
   if s == 'n':
     continue
   elif s == 's':
     os.system("say \"{0}\"".format(content))
+  elif s == 'y':
+    uuid = uuid.uuid1()
+    os.makedirs(data + '/' + uuid.hex)
+    out = open(data + '/' + uuid.hex + '/message.txt')
+
 
