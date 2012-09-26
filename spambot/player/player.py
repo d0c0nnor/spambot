be2071e2892b1fae43dae26bdd99fb8c9997b1fd
d0c0nnor <dannyoc@gmail.com>
2012-09-26 09:30:37 +0100

    Updated player script

--- spambot/player/player.py
+++ spambot/player/player.py
@@ -1,36 +1,18 @@
 import subprocess, threading
+
 from spambot import spam
 from spambot.website import settings
+from multiprocessing import Process
 import time
 import logging
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger(__name__)
 
-class Command(object):
-    def __init__(self, cmd):
-        self.cmd = cmd
-        self.process = None
-
-    def run(self, timeout):
-        def target():
-            logging.info('Thread started')
-            self.process = subprocess.Popen(self.cmd, shell=False)
-            self.process.communicate()
-            logging.info('Thread finished')
-
-        thread = threading.Thread(target=target)
-        thread.start()
-
-        thread.join(timeout)
-        if thread.is_alive():
-            log.error('Terminating process')
-            self.process.terminate()
-            thread.join()
-        log.info("Returned with %s" % self.process.returncode)
+TIMEOUT = 10*1000
 
 def _play_command(url):
-    return Command(["mplayer", "-ao", "jack:name=spambot", url])
+    return ["mplayer", '-ao', 'jack:name=spambot', url]
 
 if __name__ == "__main__":
     
@@ -39,10 +21,18 @@ if __name__ == "__main__":
             recording = spam.get_next_recording()
             log.info("Playing %s" % (recording["spam_id"]))
             mp3_url = "%s.mp3" % recording["url"]
-            command = _play_command(mp3_url)
-            command.run(timeout=5*60)
-            log.info("Sleeping for 10 secs before next play")
-            time.sleep(10)
+            p = subprocess.Popen(_play_command(mp3_url))
+
+            starttime = time.time()
+            while((time.time() - starttime) < TIMEOUT and p.poll() is None):
+                time.sleep(1)
+
+            if not p.poll():
+                log.error("Appears to be hung .. terminating process")
+                p.terminate()
+            
+            log.info("Sleeping for 5 secs before next play")
+            time.sleep(5)
         except:
             log.exception("An error occurred during playback!")
             
