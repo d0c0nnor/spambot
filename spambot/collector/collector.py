f1c1152100febf2e3b74568a96699ef2815e906a
d0c0nnor <dannyoc@gmail.com>
2012-09-26 09:41:08 +0100

    Made collector more robust

--- spambot/collector/collector.py
+++ spambot/collector/collector.py
@@ -7,6 +7,8 @@ import sys
 import md5
 import base64
 import uuid
+import logging
+import time
 
 usage = """
 usage:
@@ -72,56 +74,65 @@ if len(sys.argv) != 2:
   sys.exit(-1)
 
 accounts = 'mail abc a aa aaa email no yes spam hi hello bye xxx x'.split(' ')
-
-
 data_folder = os.path.abspath(sys.argv[1])
 
-#os.chdir(path)
-for account in accounts:
-  print "connecting to account {0}".format(account)
-  M = poplib.POP3('pop.mailinator.com')
-  print "connected"
-  M.user(account)
-  M.pass_('1234')
-  numMessages = len(M.list()[1])
-  for i in range(numMessages):
-    print "retrieving message #{0}".format(i+1)
-    lines = M.retr(i+1)[1]
-    content = '\n'.join(lines)
-    payload = get_msg_payload(content)
-    if payload != None and type(payload) is str:
-      content = cleanup(payload)
-      if content.count(' ') == 0:
-        content = base64.b64decode(content)
-        content = get_msg_payload(content)
-        content = cleanup(content)
-      if content.count(' ') == 0:
-        continue
+if __name__ == "__main__":
 
-      content_white_spaces = content.count(' ')
-      content_len = len(content)
-      ratio = float(content_white_spaces) / content_len
-      if ratio < 0.1:
-        continue
-      if len(content) > 1000:
-        continue
+  while(True):
+      #os.chdir(path)
+    for account in accounts:
 
-      content_hash = hash(content)
-      destination_folder = data_folder + '/' + content_hash
-      destination_file = destination_folder + '/' + message_file_name
-      destination_topic_file = destination_folder + '/' + topic_file_name
-      if not os.path.exists(destination_folder):
-        print "saving {0}".format(content_hash)
-        print content
-        print "ratio: {0}/{1}={2}".format(content_white_spaces, content_len, ratio)
-        os.makedirs(destination_folder)
-        f = open(destination_file, 'w')
-        f.write(content)
-        f.close()
-        f = open(destination_topic_file, 'w')
-        f.write(get_topic(content))
-        f.close()
-#    raw_input("press return")
+      # no need to kill the mailinator server!
+      time.sleep(2)
+      
+      try: 
+      
+        print "connecting to account {0}".format(account)
+        M = poplib.POP3('pop.mailinator.com')
+        print "connected"
+        M.user(account)
+        M.pass_('1234')
+        numMessages = len(M.list()[1])
+        for i in range(numMessages):
+          print "retrieving message #{0}".format(i+1)
+          lines = M.retr(i+1)[1]
+          content = '\n'.join(lines)
+          payload = get_msg_payload(content)
+          if payload != None and type(payload) is str:
+            content = cleanup(payload)
+            if content.count(' ') == 0:
+              content = base64.b64decode(content)
+              content = get_msg_payload(content)
+              content = cleanup(content)
+            if content.count(' ') == 0:
+              continue
+
+            content_white_spaces = content.count(' ')
+            content_len = len(content)
+            ratio = float(content_white_spaces) / content_len
+            if ratio < 0.1:
+              continue
+            if len(content) > 1000:
+              continue
+
+            content_hash = hash(content)
+            destination_folder = data_folder + '/' + content_hash
+            destination_file = destination_folder + '/' + message_file_name
+            destination_topic_file = destination_folder + '/' + topic_file_name
+            if not os.path.exists(destination_folder):
+              print "saving {0}".format(content_hash)
+              print content
+              print "ratio: {0}/{1}={2}".format(content_white_spaces, content_len, ratio)
+              os.makedirs(destination_folder)
+              f = open(destination_file, 'w')
+              f.write(content)
+              f.close()
+              f = open(destination_topic_file, 'w')
+              f.write(get_topic(content))
+              f.close()
+
+      except Exception, e:
+        logging.exception("Couldn't retrieve mail", e)
 
 """
   s = raw_input("press 'n' for next, 's' for speak...\n\n\n")
